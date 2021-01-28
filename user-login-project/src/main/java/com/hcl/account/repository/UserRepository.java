51c0eb13846d3904ad03dc07db4e85fd76dffdad
Spangevan <ewlson23@gmail.com>
2021-01-28 14:59:07 -0500

    added user and role repository interfaces

---
+++ user-login-project/src/main/java/com/hcl/account/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hcl.account.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hcl.account.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByUsername(String username);
+
+}
